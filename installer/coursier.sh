959377746e3eac0ff08f665b8cbe67311f5040f0
Atsushi Miyazaki <ki929.oa95cp@gmail.com>
2025-01-30 10:00:58 +0900

    remove unused ones

--- installer/coursier.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-curl -fLo cs https://git.io/coursier-cli-"$(uname | tr LD ld)"
-chmod +x cs
-./cs install cs
-rm cs
--- installer/sdkman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-curl -s "https://get.sdkman.io" | bash
-
-echo source '"$HOME/.sdkman/bin/sdkman-init.sh"' >> $HOME/.zshrc
