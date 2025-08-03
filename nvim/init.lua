aba219e54037f88bac4f3c058b327ab10894d994
Atsushi Miyazaki <ki929.oa95cp@gmail.com>
2025-08-03 19:24:41 +0900

    update neovim to nightly (0.12)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,51 +1,66 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = ";"
-vim.g.nvim_tree_respect_buf_cwd = 1
-vim.opt.autochdir = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 2
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.winborder = "rounded"
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "rust",
-  callback = function()
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
-    vim.bo.softtabstop = 2
-    vim.bo.expandtab = true
-  end,
+vim.g.mapleader = ";"
+
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<leader>n", ":noh<CR>")
+vim.keymap.set("t", "<C-j><C-k>", "<C-\\><C-n><C-w>h")
+
+vim.pack.add({
+	{ src = "https://github.com/Mofiqul/vscode.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/folke/snacks.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/coder/claudecode.nvim" }
 })
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+require("snacks").setup {
+	indent = { enabled = true },
+	picker = { enabled = true },
+	notifier = { enabled = true },
+	quickfile = { enabled = true },
+	statuscolumn = { enabled = true },
+	zen = { enabled = true },
+}
+vim.keymap.set("n", "<leader>e", ":lua Snacks.explorer()<CR>", { silent = true} )
+vim.keymap.set("n", "<leader>ff", ":lua Snacks.picker.files()<CR>", { silent = true} )
+vim.keymap.set("n", "<leader>bf", ":lua Snacks.picker.buffers()<CR>", { silent = true} )
+vim.keymap.set("n", "<leader>gf", ":lua Snacks.picker.grep()<CR>", { silent = true} )
+vim.keymap.set("n", "<leader>z", ":lua Snacks.zen()<CR>", { silent = true} )
 
-if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
+require("claudecode").setup {
+	terminal_cmd = "~/.claude/local/claude"
+}
+vim.keymap.set("n", "<leader>ac", "<cmd>ClaudeCode<CR>")
 
-vim.opt.rtp:prepend(lazypath)
+vim.lsp.enable({
+	"lua_ls",
+	"gopls"
+})
 
-local lazy_config = require "configs.lazy"
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
-  },
+vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev)
 
-  { import = "plugins" },
-}, lazy_config)
+vim.cmd("colorscheme vscode")
+vim.cmd(":hi statusline guibg=NONE")
 
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "nvchad.autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+	callback = function()
+		vim.cmd("TSEnable highlight")
+	end,
+})
